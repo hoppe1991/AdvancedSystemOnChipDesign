b6d0e454bf7338c2bf4096f0d77f6c20f5070272
hoppe1991 <carsten.hoppe@tuhh.de>
2017-01-16 10:56:03 +0100

    Update asm-file for stalling test

--- asm/stallingTestWithNop.asm
+++ asm/stallingTestWithNop.asm
@@ -2,8 +2,30 @@
 29005572
 .text
 addi	$t2,	$zero,	7	# Initialize reg 10 with 7
+nop
+nop
+nop
+nop
+nop
+bne $t2, $zero, L1
+addi $t2, $t7, 1
+addi $t2, $t6, 1
+addi $t2, $t5, 1
+L1:
+nop
+sub $t2, $t2, $zero
 addi	$t3,	$zero,	2	# Initialize reg 11 with 2
+nop
+nop
+nop
+nop
+nop
+nop
 addi	$t4,	$zero,	9	# Initialize reg 12 with 9
+addi 	$t4,    $t4,    4
+nop
+nop
+nop
 addi	$t5,	$zero,	77	# Initialize reg 13 with 77
 subu	$sp,	$sp,	4	# Decrease stack pointer by 4 byte
 sw	$t2,	0($sp)		# Write value 7 at current stack pointer

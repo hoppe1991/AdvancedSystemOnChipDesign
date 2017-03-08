af38d55ca1cfae83e33b4095bf47f58bd8db6f76
hoppe1991 <carsten.hoppe@tuhh.de>
2017-03-08 10:16:16 +0100

    [Task4] Update of bat simulation.

--- hdl/run_ghdl_cacheTestbench.bat
+++ hdl/run_ghdl_cacheTestbench.bat
@@ -13,33 +13,33 @@ java -jar ./../../../Mars4_5.jar a dump .data HexText ../dmem/%1.dmem ../asm/%1.
 
 echo.
 echo ++++++++++ check syntax of the vhdl file gates.vhd ++++++++++
-ghdl -a -g -O3 --ieee=synopsys --workdir=work convertMemFiles.vhd
+ghdl -a -g -O3 --ieee=synopsys --workdir=workB convertMemFiles.vhd
 echo.
 echo ++++++++++ create an executable for the testbench ++++++++++
-ghdl -e -g -O3 --ieee=synopsys --workdir=work convertMemFiles
+ghdl -e -g -O3 --ieee=synopsys --workdir=workB convertMemFiles
 echo.
 echo ++++++++++ run the executable ++++++++++
-ghdl -r -g -O3 --ieee=synopsys --workdir=work convertMemFiles -gDFileName="../dmem/%1" -gIFileName="../imem/%1"
+ghdl -r -g -O3 --ieee=synopsys --workdir=workB convertMemFiles -gDFileName="../dmem/%1" -gIFileName="../imem/%1"
 
 echo ++++++++++ Create files for cache BRAMs +++++++++++++++++++++++
 echo.
-ghdl -a -g -O3 --ieee=synopsys --workdir=work cache_pkg.vhd creatorOfCacheFiles.vhd
+ghdl -a -g -O3 --ieee=synopsys --workdir=workB cache_pkg.vhd creatorOfCacheFiles.vhd
 echo.
 echo ++++++++++ create an executable for the testbench ++++++++++
-ghdl -e -g -O3 --ieee=synopsys --workdir=work creatorOfCacheFiles
+ghdl -e -g -O3 --ieee=synopsys --workdir=workB creatorOfCacheFiles
 echo.
 echo ++++++++++ run the executable ++++++++++
-ghdl -r -g -O3 --ieee=synopsys --workdir=work creatorOfCacheFiles -gTag_Filename="../imem/tag%1" -gData_Filename="../imem/data%1"
+ghdl -r -g -O3 --ieee=synopsys --workdir=workB creatorOfCacheFiles -gTag_Filename="../imem/tag%1" -gData_Filename="../imem/data%1"
 
 
 
 @echo off
 echo.
 echo ++++++++++ add files in the work design library ++++++++++
-ghdl -i -g -O3 --ieee=synopsys --workdir=workB cache_tb.vhd cacheController.vhd directMappedCache_tb.vhd directMappedCache.vhd bram.vhd mips_pkg.vhd casts.vhd mainMemoryController.vhd cache_pkg.vhd
+ghdl -i -g -O3 --ieee=synopsys --workdir=workB mips_pkg.vhd casts.vhd cache_pkg.vhd bram.vhd directMappedCacheController.vhd mainMemoryController.vhd mainMemory.vhd directMappedCache.vhd cacheController.vhd cache.vhd cache_tb.vhd 
 echo.
 echo ++++++++++ analyze automatically outdated files and create an executable ++++++++++
 ghdl -m -g -O3 --ieee=synopsys --workdir=workB cache_tb
 echo.
 echo ++++++++++ run the executable for 15us and save all waveforms ++++++++++
-ghdl -r -g -O3 --ieee=synopsys --workdir=workB cache_tb --stop-time=40us  --wave=../sim/cacheTest.ghw -gMAIN_MEMORY_FILENAME="../imem/%1"
+ghdl -r -g -O3 --ieee=synopsys --workdir=workB cache_tb --stop-time=100000ns  --wave=../sim/cacheTestbench.ghw -gMAIN_MEMORY_FILENAME="../imem/%1"

718dbba7682692723152f0f0f466d6acdc9561b4
hoppe1991 <carsten.hoppe@tuhh.de>
2017-03-23 10:48:32 +0100

    [Task4] Rename scipt. Change windows bat-script regarding 2-way cache. TODO Create sh script regarding 2-way cache.

--- hdl/run_ghdl_task4_2wayCacheTestbench_windows.bat
+++ hdl/run_ghdl_task4_2wayCacheTestbench_windows.bat
@@ -9,7 +9,6 @@ GOTO :eof
 REM Folder name of work directory.
 SET location="workC"
 
-
 REM Set generic variables for 2-way associative cache.
 SET replacementStrategy=LRU_t
 SET mainMemoryFilename="../imem/%1"
@@ -17,24 +16,21 @@ SET dataFilenameCache1="../imem/data%1_cache1"
 SET dataFilenameCache2="../imem/data%1_cache2"
 SET tagFilenameCache1="../imem/tag%1_cache1"
 SET tagFilenameCache2="../imem/tag%1_cache2"
-SET fileExtension=".imem" 
-
-
-
+SET fileExtension=".imem"
 
 REM Remove the work directory if it already exists.
 echo ++++++++++ Create work folder +++++++++++++++++++++++++++++++++
-if exist %location% ( 
+if exist %location% (
 	echo "Remove work directory."
 	rmdir /s /q %location%
-) 
+)
 
 REM Create new work folder if it does not exist.
 if not exist %location% (
 	echo "Create work directory."
 	mkdir %location%
  )
- 
+
 echo ++++++++++ assemble the MIPS program (imem and dmem) ++++++++++
 java -jar ./../../../Mars4_5.jar a dump .text HexText ../imem/%1.imem ../asm/%1.asm
 java -jar ./../../../Mars4_5.jar a dump .data HexText ../dmem/%1.dmem ../asm/%1.asm
@@ -61,7 +57,7 @@ ghdl -r -g -O3 --ieee=synopsys --workdir=%location% creatorOfTwoWayCacheFiles -g
 @echo off
 echo.
 echo ++++++++++ add files in the work design library ++++++++++
-ghdl -i -g -O3 --ieee=synopsys --workdir=%location% mips_pkg.vhd casts.vhd cache_pkg.vhd bram.vhd directMappedCacheController.vhd mainMemoryController.vhd mainMemory.vhd directMappedCache.vhd cacheController.vhd twoWayAssociativeCacheController.vhd twoWayAssociativeCache.vhd twoWayAssociativeCache_tb.vhd 
+ghdl -i -g -O3 --ieee=synopsys --workdir=%location% mips_pkg.vhd casts.vhd cache_pkg.vhd bram.vhd directMappedCacheController.vhd mainMemoryController.vhd mainMemory.vhd directMappedCache.vhd cacheController.vhd twoWayAssociativeCacheController.vhd twoWayAssociativeCache.vhd twoWayAssociativeCache_tb.vhd
 echo.
 echo ++++++++++ analyze automatically outdated files and create an executable ++++++++++
 ghdl -m -g -O3 --ieee=synopsys --workdir=%location% twoWayAssociativeCache_tb

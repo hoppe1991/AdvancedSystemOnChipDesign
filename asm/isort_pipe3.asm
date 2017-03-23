e964c46c6d54643d352f3eec2c6da9f5b0672acb
Unknown <hendrikmzf@gmail.com>
2017-03-23 09:30:43 +0100

    [Task3] removed nops isort_pipe3
    mips was tested after clean up, program fine!

--- asm/isort_pipe3.asm
+++ asm/isort_pipe3.asm
@@ -12,10 +12,10 @@ val:        .word 20, 30, 10, 40, 50, 60, 30, 25, 10, 5 # Liste
   nop
   
 main:      jal isort
-			nop
+
 		   
 end:       j end 
-		nop         
+       
 
 isort:                                                  # isort()
             # reg usage
@@ -28,79 +28,57 @@ for_init:   addi $2, $0, 1
             lui  $1, 0x1001     # lw   $7, len     addr = 0x10010000
             lw   $7, 0($1)
             addi $1, $0, 1      # array length (10) in $1
-            nop
-            nop 
+
             sub  $7, $7, $1
             lui  $1, 0x1001     # la   $8, val  pseudo ins. for load address
-            nop
-            nop
+
             ori  $8, $1, 4      # reg[8] = addr(val[0]); 
             addi $9, $0, 4
 
 for_loop:   slt  $1, $7, $2     # bgt  $2, $7, end_for
-            nop
-            nop
+
             bne  $1, $0, end_for #21
-            nop
-            nop 
+
             addi $1, $0, 1      # sub  $3, $2, 1
-            nop
-            nop
+
             sub  $3, $2, $1
             mul  $6, $2, $9     # addr(val[i]) = addr(val[0])+i*(size(int))
-            nop
-            nop
+
             add  $6, $8, $6
-            nop
-            nop
+
             lw   $4, 0($6)
-            nop
-            nop
+
  
 while_init: slti $1, $3, 0     # blt  $3, 0,  end_while
-            nop
-            nop
+
             bne  $1, $0, end_while
-            nop
-            nop
+
             mul  $6, $3, $9    # addr(val[i]) = addr(val[0])+i*(size(int))
-            nop
-            nop
+
             add  $6, $8, $6
-            nop
-            nop
+
             lw   $5, 0($6)
-            nop
-            nop
+
             slt  $1, $4, $5    # ble  $5, $4, end_while
-            nop
-            nop
+
             beq  $1, $0, end_while
-            nop
-            nop
+
             sw   $5, 4($6)
             addi $1, $0, 1     # sub  $3, $3, 1
-            nop
-            nop  
+
             sub  $3, $3, $1
             j            while_init
-            nop
-            nop
+
 
 end_while:  mul  $6, $3, $9    # addr(val[i]) = addr(val[0])+i*(size(int))
-            nop
-            nop
+
             add  $6, $8, $6
-            nop
-            nop
+
             sw   $4, 4($6)
             addi $2, $2, 1
             j            for_loop
-            nop
-            nop
+
 end_for:    j            return
-            nop
-            nop
+
 return:     jr   $ra
-            nop
-            nop
+
